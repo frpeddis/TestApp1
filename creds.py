e1700093929d8cd3a0ce9f65a953aec4c3b5f800
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-14 10:49:45 +0200

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+OPENAI_API_KEY="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"
+
---
+++ just_api_ok.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import openai
+import creds
+
+# Set your OpenAI API key
+openai.api_key = creds.OPENAI_API_KEY
+
+def generate_news(selected_date):
+    prompt = f"What happened on {selected_date}?\nGive me a good news with a 😄, a neutral news with a 😐, and a bad news with a 😔. Do not mention good news, neutral news, bad news: just use the icons. Insert related Wikipedia links."
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=150,
+        temperature=0.7
+    )
+
+    return response.choices[0].text.strip()
+
+selected_date = "1960-03-09"  # Replace this with your desired date
+news_summary = generate_news(selected_date)
+print(news_summary)
+
