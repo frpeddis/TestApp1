9935f5da0f830f726bd53811c344d5a92ba0eb81
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-17 09:45:44 +0200

    Update Random_Date.py

--- Random_Date.py
+++ Random_Date.py
@@ -70,12 +70,12 @@ if check_button:
     if selected_day_of_week == day_of_week:
         st.success(day_of_week + " OK :thumbsup:")
         news_summary = generate_news(selected_date)
-        st.title("Please verify, but according to ChatGPT in that period...")
+        st.header("Please verify, but according to ChatGPT in that period...")
         st.write(news_summary)         
     else:
         st.error(day_of_week + " WRONG!!!")
         news_summary = generate_news(selected_date)
-        st.title("Please verify, but according to ChatGPT in that period...")
+        st.header("Please verify, but according to ChatGPT in that period...")
         st.write(news_summary)
         
 
