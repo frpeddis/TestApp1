662bac838c5b470243f22c185affd0f16b13c710
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-17 22:09:42 +0200

    Update new_slider.py

--- new_slider.py
+++ new_slider.py
@@ -16,8 +16,8 @@ def generate_news(selected_date):
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=150,
-        temperature=0.7
+        max_tokens=200,
+        temperature=0.5
     )
 
     return response.choices[0].text.strip()
