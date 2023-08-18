4918c537c5a7605f23d1e0d1bf3388607e09e5bf
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-18 18:19:37 +0200

    Update Random_Date.py

--- Random_Date.py
+++ Random_Date.py
@@ -78,7 +78,7 @@ if check_button:
         st.success(day_of_week + " OK :thumbsup:")
                 
     else:
-        st.error(day_of_week + " was the right date :coffee:" )
+        st.error(" Oh nooo! the right day was :coffee:" + day_of_week )
         com.iframe("https://lottie.host/?file=380d3ff9-0c30-4a96-b25b-7eeb8868bfeb/vnvhMZFQ8j.json")
 
         
