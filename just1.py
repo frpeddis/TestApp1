65a076fa4cd717cdcda733426b441ba85ae50582
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-18 15:04:37 +0200

    Update just1.py

--- just1.py
+++ just1.py
@@ -1,34 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[ ]:
-
-
-import openai
-import creds
-import os
 import streamlit as st
+from streamlit.components.v1 import components
 
-# Set your OpenAI API key
-#openai.api_key = "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"
-#openai.api_key = creds.OPENAI_API_KEY
+def main():
+    st.title("Lottie Animation in Streamlit")
 
-#os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
-openai.api_key = st.secrets['OPENAI_API_KEY']
+    # Load the Lottie JSON animation
+    animation_json = open("animation.json", "r").read()
 
-def generate_news(selected_date):
-    prompt = f"What happened on {selected_date}?\nGive me a good news with a 😄, a neutral news with a 😐, and a bad news with a 😔. Insert related Wikipedia links."
+    # Display the Lottie animation
+    components.html(animation_json, width=400, height=400)
 
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=150,
-        temperature=0.7
-    )
-
-    return response.choices[0].text.strip()
-
-selected_date = "1968-03-09"  # Replace this with your desired date
-news_summary = generate_news(selected_date)
-print(news_summary)
-
+if __name__ == "__main__":
+    main()
