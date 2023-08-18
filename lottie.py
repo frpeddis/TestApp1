d5a8b7c09b17d5eb0f684363d92aee3f154bb6a8
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-18 15:44:26 +0200

    Add files via upload

---
+++ lottie.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+from streamlit.components.v1 import components
+
+def main():
+    st.title("Lottie Animation in Streamlit")
+
+    # Load the Lottie JSON animation
+    animation_json = open("animation.json", "r").read()
+
+    # Display the Lottie animation
+    components.html(animation_json, width=400, height=400)
+
+if __name__ == "__main__":
+    main()
+
