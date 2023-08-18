9873629b901450f3ebe658cbd6c3720a88398c75
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-18 17:45:25 +0200

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -1,22 +1,4 @@
-import json
+
 import streamlit as st
-import os
-from streamlit_lottie import st_lottie
-  
-path = "~/Download/animation.json"
-with open(path,"r") as file:
-    url = json.load(file)
-  
-  
-  
-st.title("Adding Lottie Animation in Streamlit WebApp")
-  
-st_lottie(url,
-    reverse=True,
-    height=400,
-    width=400,
-    speed=1,
-    loop=True,
-    quality='high',
-    key='Car'
-)
+import streamlit.components.v1 as com
+com.iframe("https://lottie.host/?file=380d3ff9-0c30-4a96-b25b-7eeb8868bfeb/vnvhMZFQ8j.json")
