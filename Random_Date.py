bee2a259d8fdde085897fea89d7b2dfa985e32a2
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-19 18:43:06 +0200

    Update Random_Date.py

--- Random_Date.py
+++ Random_Date.py
@@ -97,4 +97,6 @@ if display_time_taken:
             news_summary = generate_news(selected_date)
             st.header("Please verify, but according to ChatGPT in that period... ")
             st.write(news_summary)
+pdf_url = "https://github.com/frpeddis/TestApp1/raw/9a5249fa93ebbb3d724c139f48c27476c30d0cd4/MAGIC%20DAY%20CALCULATOR%20ADVENTURE.pdf"
+st.markdown(f"[Download MAGIC DAY CALCULATOR ADVENTURE!]({pdf_url})")
 
