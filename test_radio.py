7f1918029c103ff0eaf318cb1e692a688f94fdbd
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-20 00:06:11 +0200

    Update test_radio.py

--- test_radio.py
+++ test_radio.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from PIL import Image
 import requests
+from PIL import Image
 from io import BytesIO
 
-url = "https://github.com/frpeddis/TestApp1/blob/8e0d72dc7d773c2061ab58791b4c7bc3fd3332ea/MAGIC%20DAY%20CALCULATOR%20ADVENTURE%201.jpg"
+url = "https://raw.githubusercontent.com/frpeddis/TestApp1/8e0d72dc7d773c2061ab58791b4c7bc3fd3332ea/MAGIC%20DAY%20CALCULATOR%20ADVENTURE%201.jpg"
 
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
