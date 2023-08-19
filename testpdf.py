5bf1375de4f90b94eb1d367d01825350d00db3d3
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-19 18:54:22 +0200

    Update testpdf.py

--- testpdf.py
+++ testpdf.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
     st.title("PDF Viewer")
 
-    pdf_url = "URL_TO_YOUR_PDF"  # Replace with the URL of your PDF
+    pdf_url = "https://github.com/frpeddis/TestApp1/blob/ad57d944ad9141a2b973d60bf7f0230dbb8c3b32/MAGIC%20DAY%20CALCULATOR%20ADVENTURE.pdf"  # Replace with the URL of your PDF
 
     st.write("Embedding PDF using iframe:")
     st.components.v1.iframe(pdf_url, width=800, height=600)
