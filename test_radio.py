38044facd4191645234d7ebc9421dabc651f65e4
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-20 00:09:37 +0200

    Update test_radio.py

--- test_radio.py
+++ test_radio.py
@@ -7,7 +7,7 @@ from io import BytesIO
 url1 = "https://raw.githubusercontent.com/frpeddis/TestApp1/8e0d72dc7d773c2061ab58791b4c7bc3fd3332ea/MAGIC%20DAY%20CALCULATOR%20ADVENTURE%201.jpg"
 response1 = requests.get(url1)
 img1 = Image.open(BytesIO(response1.content))
-st.image(img1, caption='Image 1 Caption', use_column_width=True)
+st.image(img1, use_column_width=True)
 
 # Second image
 url2 = "https://raw.githubusercontent.com/frpeddis/TestApp1/7f1918029c103ff0eaf318cb1e692a688f94fdbd/MAGIC%20DAY%20CALCULATOR%20ADVENTURE%202.jpg"
