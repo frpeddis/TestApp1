0745e7cd26a9be3a40dcf476c31463c4aba9d872
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-19 18:36:46 +0200

    Update testpdf.py

--- testpdf.py
+++ testpdf.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 def main():
-    st.title("PDF Downloader")
+    #st.title("PDF Downloader")
 
     pdf_url = "https://github.com/frpeddis/TestApp1/raw/9a5249fa93ebbb3d724c139f48c27476c30d0cd4/MAGIC%20DAY%20CALCULATOR%20ADVENTURE.pdf"
 
-    st.write("Click the link below to download the PDF:")
+    st.write("Click the link below to download how to guide:")
     st.markdown(f"[Download PDF]({pdf_url})")
 
 if __name__ == "__main__":
