e8d3d69648acc4c661baf380385354de404c9205
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-19 18:08:45 +0200

    Update testpdf.py

--- testpdf.py
+++ testpdf.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import fitz  # PyMuPDF
 
 # Title
 st.title("PDF Viewer")
@@ -6,7 +7,15 @@ st.title("PDF Viewer")
 # PDF URL
 pdf_url = "https://github.com/frpeddis/TestApp1/raw/93b1fee0e377a121c753ff8d33c46227d11616b4/MAGIC%20DAY%20CALCULATOR%20ADVENTURE.pdf"
 
-# Display PDF using iframe
-st.markdown(f'<iframe src="{pdf_url}" width="800" height="600"></iframe>', unsafe_allow_html=True)
+# Display PDF using PyMuPDF
+pdf_document = fitz.open(pdf_url, filetype="pdf")
+num_pages = pdf_document.page_count
+page_number = st.number_input("Enter page number:", min_value=1, max_value=num_pages, value=1)
+
+page = pdf_document.load_page(page_number - 1)
+image = page.get_pixmap(matrix=fitz.Matrix(3, 3))  # Adjust the matrix values for zoom
+
+st.image(image)
+
 
 
