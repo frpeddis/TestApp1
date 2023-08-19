060daa3f9cec7a5d459252c23602dd1147093fc1
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-19 18:38:05 +0200

    Update testpdf.py

--- testpdf.py
+++ testpdf.py
@@ -5,8 +5,8 @@ def main():
 
     pdf_url = "https://github.com/frpeddis/TestApp1/raw/9a5249fa93ebbb3d724c139f48c27476c30d0cd4/MAGIC%20DAY%20CALCULATOR%20ADVENTURE.pdf"
 
-    st.write("Click the link below to download how to guide:")
-    st.markdown(f"[Download PDF]({pdf_url})")
+    #st.write("Click the link below to download how to guide:")
+    st.markdown(f"[Download "How to calculate"]({pdf_url})")
 
 if __name__ == "__main__":
     main()
