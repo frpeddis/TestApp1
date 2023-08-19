f48dcfadc1a54e9079b2ae3fad1db4b78f620cb1
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-20 00:08:08 +0200

    Update test_radio.py

--- test_radio.py
+++ test_radio.py
@@ -3,9 +3,14 @@ import requests
 from PIL import Image
 from io import BytesIO
 
-url = "https://raw.githubusercontent.com/frpeddis/TestApp1/8e0d72dc7d773c2061ab58791b4c7bc3fd3332ea/MAGIC%20DAY%20CALCULATOR%20ADVENTURE%201.jpg"
+# First image
+url1 = "https://raw.githubusercontent.com/frpeddis/TestApp1/8e0d72dc7d773c2061ab58791b4c7bc3fd3332ea/MAGIC%20DAY%20CALCULATOR%20ADVENTURE%201.jpg"
+response1 = requests.get(url1)
+img1 = Image.open(BytesIO(response1.content))
+st.image(img1, caption='Image 1 Caption', use_column_width=True)
 
-response = requests.get(url)
-img = Image.open(BytesIO(response.content))
-
-st.image(img, caption='Image Caption', use_column_width=True)
+# Second image
+url2 = "https://raw.githubusercontent.com/frpeddis/TestApp1/7f1918029c103ff0eaf318cb1e692a688f94fdbd/MAGIC%20DAY%20CALCULATOR%20ADVENTURE%202.jpg"
+response2 = requests.get(url2)
+img2 = Image.open(BytesIO(response2.content))
+st.image(img2, caption='Image 2 Caption', use_column_width=True)
