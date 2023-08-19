ad57d944ad9141a2b973d60bf7f0230dbb8c3b32
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-19 18:53:38 +0200

    Update testpdf.py

--- testpdf.py
+++ testpdf.py
@@ -6,7 +6,7 @@ def main():
     pdf_url = "URL_TO_YOUR_PDF"  # Replace with the URL of your PDF
 
     st.write("Embedding PDF using iframe:")
-    st.write(f'<iframe src="{pdf_url}" width="800" height="600"></iframe>', unsafe_allow_html=True)
+    st.components.v1.iframe(pdf_url, width=800, height=600)
 
 if __name__ == "__main__":
     main()
