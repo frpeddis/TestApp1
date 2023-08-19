8fe60edf17b334a7701a41b23f047f82ace6fe45
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-20 00:17:58 +0200

    Update Random_Date.py

--- Random_Date.py
+++ Random_Date.py
@@ -5,6 +5,25 @@ from datetime import datetime, timedelta
 import openai
 import time
 import streamlit.components.v1 as com
+import requests
+from PIL import Image
+from io import BytesIO
+
+# Checkbox to toggle image display
+show_images = st.checkbox("Show me how to calculate!")
+
+if show_images:
+    # First image
+    url1 = "https://raw.githubusercontent.com/frpeddis/TestApp1/8e0d72dc7d773c2061ab58791b4c7bc3fd3332ea/MAGIC%20DAY%20CALCULATOR%20ADVENTURE%201.jpg"
+    response1 = requests.get(url1)
+    img1 = Image.open(BytesIO(response1.content))
+    st.image(img1, use_column_width=True)
+
+    # Second image
+    url2 = "https://raw.githubusercontent.com/frpeddis/TestApp1/7f1918029c103ff0eaf318cb1e692a688f94fdbd/MAGIC%20DAY%20CALCULATOR%20ADVENTURE%202.jpg"
+    response2 = requests.get(url2)
+    img2 = Image.open(BytesIO(response2.content))
+    st.image(img2, use_column_width=True)
 
 openai.api_key = st.secrets["API_KEY"]
 
