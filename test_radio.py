fc52ba1111f27beeea324c1310c5cd9935f9eea8
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-20 00:02:37 +0200

    Update test_radio.py

--- test_radio.py
+++ test_radio.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-image = "path_to_your_image.jpg"  # Replace with the actual path to your image file
+image = "https://github.com/frpeddis/TestApp1/blob/8e0d72dc7d773c2061ab58791b4c7bc3fd3332ea/MAGIC%20DAY%20CALCULATOR%20ADVENTURE%201.jpg"  # Replace with the actual path to your image file
 st.image(image, caption='Image Caption', use_column_width=True)
 
 
