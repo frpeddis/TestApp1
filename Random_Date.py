721aa5b4f23a535e8e27933dfbb8a23312e3777c
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-20 10:01:36 +0200

    Update Random_Date.py

--- Random_Date.py
+++ Random_Date.py
@@ -9,21 +9,22 @@ import requests
 from PIL import Image
 from io import BytesIO
 
+
 # Checkbox to toggle image display
-show_images = st.checkbox("Show me how to calculate!")
+show_images = st.checkbox("Show Images")
 
 if show_images:
-    # First image
-    url1 = "https://raw.githubusercontent.com/frpeddis/TestApp1/8e0d72dc7d773c2061ab58791b4c7bc3fd3332ea/MAGIC%20DAY%20CALCULATOR%20ADVENTURE%201.jpg"
-    response1 = requests.get(url1)
-    img1 = Image.open(BytesIO(response1.content))
-    st.image(img1, use_column_width=True)
-
-    # Second image
-    url2 = "https://raw.githubusercontent.com/frpeddis/TestApp1/7f1918029c103ff0eaf318cb1e692a688f94fdbd/MAGIC%20DAY%20CALCULATOR%20ADVENTURE%202.jpg"
-    response2 = requests.get(url2)
-    img2 = Image.open(BytesIO(response2.content))
-    st.image(img2, use_column_width=True)
+    image_links = [
+        "https://raw.githubusercontent.com/frpeddis/TestApp1/712049d40da877b81ab0eaa7bb8387cf532dfe55/MAGIC%20DAY%20CALCULATOR%20ADVENTURE_1.jpg",
+        "https://raw.githubusercontent.com/frpeddis/TestApp1/712049d40da877b81ab0eaa7bb8387cf532dfe55/MAGIC%20DAY%20CALCULATOR%20ADVENTURE_2.jpg",
+        "https://raw.githubusercontent.com/frpeddis/TestApp1/712049d40da877b81ab0eaa7bb8387cf532dfe55/MAGIC%20DAY%20CALCULATOR%20ADVENTURE_3.jpg",
+        "https://raw.githubusercontent.com/frpeddis/TestApp1/712049d40da877b81ab0eaa7bb8387cf532dfe55/MAGIC%20DAY%20CALCULATOR%20ADVENTURE_4.jpg"
+    ]
+    
+    for i, link in enumerate(image_links):
+        response = requests.get(link)
+        img = Image.open(BytesIO(response.content))
+        st.image(img, use_column_width=True)
 
 openai.api_key = st.secrets["API_KEY"]
 
