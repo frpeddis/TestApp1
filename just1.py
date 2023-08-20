1c7722ea3613936b016a6eda99b4bc46ddb541b5
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-20 19:32:51 +0200

    Update just1.py

--- just1.py
+++ just1.py
@@ -8,15 +8,15 @@ st.title("Calculate Day of the Week")
 selected_date = st.date_input("Step 1: Select a date")
 
 if selected_date:
-    st.write("Step 1: Selected Date:", selected_date.strftime("%d-%b-%Y"))
+    st.write("Selected Date:", selected_date.strftime("%d-%b-%Y"))
 
     # Step 2: Take the last 2 digits of the year
     year_last_2_digits = selected_date.year % 100
-    st.write("Step 2: Last 2 digits of the year:", year_last_2_digits)
+    st.write("Last 2 digits of the year:", year_last_2_digits)
 
     # Step 3: Divide the year number by 4 and add it
     year_divided_by_4 = year_last_2_digits // 4
-    st.write("Step 3: Integer part of year divided by 4:", year_divided_by_4)
+    st.write("Integer part of year divided by 4:", year_divided_by_4)
     subtotal = year_last_2_digits + year_divided_by_4
     st.write("    Sum of the above:", subtotal)
 
@@ -37,24 +37,24 @@ if selected_date:
         3: 4, 4: 0, 5: 2, 6: 5, 7: 0, 8: 3, 9: 6, 10: 1, 11: 4, 12: 6
     }
     month_coefficient = month_coefficients[selected_date.month]
-    st.write("Step 5: Month Coefficient value:", month_coefficient)
+    st.write("Month Coefficient value:", month_coefficient)
     subtotal += month_coefficient
     st.write("    Sum of the above:", subtotal)
 
     # Step 6: Add the day of the month
     day_of_month = selected_date.day
-    st.write("Step 6: Day of the month:", day_of_month)
+    st.write("Day of the month:", day_of_month)
     subtotal += day_of_month
     st.write("    Sum of the above:", subtotal)
 
     # Step 7: Divide the subtotal by 7 and find the remainder
     remainder = subtotal % 7
-    st.write("Step 7: Remainder after dividing by 7:", remainder)
+    st.write("Remainder after dividing by 7:", remainder)
 
     # Step 8: Find the day of the week
     days_of_week = [
         "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
     ]
     day_of_week = days_of_week[remainder]
-    st.write("Step 8: Calculated Day of the Week:", day_of_week)
+    st.write("-> Calculated Day of the Week:", day_of_week)
 
