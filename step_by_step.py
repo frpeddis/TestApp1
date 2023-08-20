53c741e790a76b878a57d4eecc76c77844496d2e
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-20 22:33:35 +0200

    Update step_by_step.py

--- step_by_step.py
+++ step_by_step.py
@@ -1,62 +1,13 @@
-import random
-import calendar
 import streamlit as st
-from datetime import datetime, timedelta
+from datetime import datetime
 
 # Streamlit app title
-st.title("What day was it ? - Random choice:sunglasses:")
+st.title("Calculate Day of the Week")
 
-# Function to calculate a random date
-def calculate_random_date():
-    start_date = datetime(1582, 10, 15)
-    end_date = datetime(2099, 12, 31)
-    return start_date + timedelta(seconds=random.randint(0, int((end_date - start_date).total_seconds())))
-
-# Check if random_date and start_time are in session state, if not, calculate and store them
-if 'random_date' not in st.session_state:
-    st.session_state.random_date = calculate_random_date()
-    
-if 'start_time' not in st.session_state:
-    st.session_state.start_time = datetime.now()
-
-if 'check_pressed' not in st.session_state:
-    st.session_state.check_pressed = False
-
-if 'time_taken' not in st.session_state:
-    st.session_state.time_taken = 0
-
-# Display the date in the format dd-mmm-yyyy
-st.write("Random Date:", st.session_state.random_date.strftime("%d-%b-%Y"))
-
-# Calculate time taken
-if not st.session_state.check_pressed:
-    time_taken = (datetime.now() - st.session_state.start_time).total_seconds()
-    display_time_taken = False
-else:
-    time_taken = st.session_state.time_taken
-    display_time_taken = True
-
-# Prompt the user to select the day of the week from a dropdown list
-selected_day_of_week = st.selectbox("Select the day of the week:", list(calendar.day_name))
-
-# Add a "Check" button to confirm the selection
-check_button = st.button("Check")
-
-if check_button:
-    st.session_state.check_pressed = True
-
-    # Confirm the day of the week selected by the user
-    day_of_week = calendar.day_name[st.session_state.random_date.weekday()]
-
-    if selected_day_of_week == day_of_week:
-        st.success(day_of_week + " OK")
-    else:
-        st.error(day_of_week + " WRONG!!!")
-################               
 # Step 1: User selects a date
-selected_date = st.session_state.random_date
+selected_date = st.date_input("Step 1: Select a date")
 
-if sselected_date:
+if selected_date:
     st.write("Step 1: Selected Date:", selected_date.strftime("%d-%b-%Y"))
 
     # Step 2: Take the last 2 digits of the year
@@ -134,15 +85,3 @@ if sselected_date:
           for remainder, day in zip(correspondence_table["Remainder"], correspondence_table["Day of the Week"])]
     ]
     st.table(formatted_correspondence_table)
-
-
-
-###########
-    # Calculate time taken to make the selection
-    st.session_state.time_taken = (datetime.now() - st.session_state.start_time).total_seconds()
-    time_taken = st.session_state.time_taken
-    display_time_taken = True
-
-# Show the amount of seconds taken
-if display_time_taken:
-    st.write("Time taken to check:", round(time_taken, 2), "seconds")
