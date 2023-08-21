a0a2175b7c26b43bb6f7c6b2ee16480e5d35a038
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-21 16:25:00 +0200

    Update just1.py

--- just1.py
+++ just1.py
@@ -12,42 +12,109 @@ def calculate_random_date():
     end_date = datetime(2099, 12, 31)
     return start_date + timedelta(seconds=random.randint(0, int((end_date - start_date).total_seconds())))
 
-# ... (rest of the code remains the same)
+# Check if random_date and start_time are in session state, if not, calculate and store them
+if 'random_date' not in st.session_state:
+    st.session_state.random_date = calculate_random_date()
 
-# Display Century Correction Table
-st.write("Step 4: Century Correction Table:")
-formatted_century_correction_table = []
-for century, correction in zip(century_correction_table["Century"], century_correction_table["Correction"]):
-    if century == (selected_date.year // 100) * 100:
-        formatted_century_correction_table.append(["**" + str(century) + "**", "**" + str(correction) + "**"])
+if 'start_time' not in st.session_state:
+    st.session_state.start_time = datetime.now()
+
+if 'check_pressed' not in st.session_state:
+    st.session_state.check_pressed = False
+
+if 'time_taken' not in st.session_state:
+    st.session_state.time_taken = 0
+
+# Display the date in the format dd-mmm-yyyy
+st.write("Random Date:", st.session_state.random_date.strftime("%d-%b-%Y"))
+
+# Calculate time taken
+if not st.session_state.check_pressed:
+    time_taken = (datetime.now() - st.session_state.start_time).total_seconds()
+    display_time_taken = False
+else:
+    time_taken = st.session_state.time_taken
+    display_time_taken = True
+
+# Prompt the user to select the day of the week from a dropdown list
+selected_day_of_week = st.selectbox("Select the day of the week:", list(calendar.day_name))
+
+# Add a "Check" button to confirm the selection
+check_button = st.button("Check")
+
+if check_button:
+    st.session_state.check_pressed = True
+
+    # Confirm the day of the week selected by the user
+    day_of_week = calendar.day_name[st.session_state.random_date.weekday()]
+
+    if selected_day_of_week == day_of_week:
+        st.success(day_of_week + " OK")
     else:
-        formatted_century_correction_table.append([str(century), str(correction)])
-st.table(formatted_century_correction_table, header=["Century", "Value"])
+        st.error(day_of_week + " WRONG!!!")
 
-# ... (rest of the code remains the same)
+    # Calculate time taken to make the selection
+    st.session_state.time_taken = (datetime.now() - st.session_state.start_time).total_seconds()
+    time_taken = st.session_state.time_taken
+    display_time_taken = True
 
-# Display Month Coefficient Table (continued)
-st.write("Step 5: Month Coefficient Table:")
-formatted_month_coefficients_table = []
-for month, coeff in month_coefficients.items():
-    if month == selected_date.strftime("%B"):
-        formatted_month_coefficients_table.append(["**" + month + "**", "**" + str(coeff) + "**"])
-    else:
-        formatted_month_coefficients_table.append([month, str(coeff)])
-st.table(formatted_month_coefficients_table, header=["Month", "Value"])
+# Show the amount of seconds taken
+if display_time_taken:
+    st.write("Time taken to check:", round(time_taken, 2), "seconds")
 
-# ... (rest of the code remains the same)
+# Step 1: User selects a date
+selected_date = st.session_state.random_date
 
-# Display Correspondence Table
-st.write("Correspondence between Remainders and Days of the Week:")
-correspondence_table = {
-    "Remainder": list(range(7)),
-    "Day of the Week": ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-}
-formatted_correspondence_table = []
-for r, d in zip(correspondence_table["Remainder"], correspondence_table["Day of the Week"]):
-    if r == remainder:
-        formatted_correspondence_table.append(["**" + str(r) + "**", "**" + d + "**"])
-    else:
-        formatted_correspondence_table.append([str(r), d])
-st.table(formatted_correspondence_table, header=["Reminder", "Day of the week"])
+if selected_date:
+    st.write("Step 1: Selected Date:", selected_date.strftime("%d-%b-%Y"))
+
+    # ... (Other steps remain unchanged)
+
+    # Display Century Correction Table
+    st.write("Step 4: Century Correction Table:")
+    century_correction_table = {
+        "Century": [1500, 1600, 1700, 1800, 1900, 2000],
+        "Correction": [0, 6, 4, 2, 0, -1]
+    }
+    formatted_century_correction_table = []
+    for century, correction in zip(century_correction_table["Century"], century_correction_table["Correction"]):
+        if century == (selected_date.year // 100) * 100:
+            formatted_century_correction_table.append(["**" + str(century) + "**", "**" + str(correction) + "**"])
+        else:
+            formatted_century_correction_table.append([str(century), str(correction)])
+    st.table(formatted_century_correction_table, headers=["Century", "Correction"])
+
+    # ... (Other steps remain unchanged)
+
+    # Display Month Coefficient Table
+    st.write("Step 5: Month Coefficient Table:")
+    month_coefficients = {
+        "January": 1 if not (selected_date.year % 4 == 0 and selected_date.month <= 2) else 0,
+        "February": 4 if not (selected_date.year % 4 == 0 and selected_date.month <= 2) else 3,
+        "March": 4, "April": 0, "May": 2, "June": 5,
+        "July": 0, "August": 3, "September": 6,
+        "October": 1, "November": 4, "December": 6
+    }
+    formatted_month_coefficients_table = []
+    for month, coeff in month_coefficients.items():
+        if month == selected_date.strftime("%B"):
+            formatted_month_coefficients_table.append(["**" + month + "**", "**" + str(coeff) + "**"])
+        else:
+            formatted_month_coefficients_table.append([month, str(coeff)])
+    st.table(formatted_month_coefficients_table, headers=["Month", "Value"])
+
+    # ... (Other steps remain unchanged)
+
+    # Display Correspondence Table
+    st.write("Correspondence between Remainders and Days of the Week Table:")
+    correspondence_table = {
+        "Remainder": list(range(7)),
+        "Day of the Week": ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    }
+    formatted_correspondence_table = []
+    for r, d in zip(correspondence_table["Remainder"], correspondence_table["Day of the Week"]):
+        if r == remainder:
+            formatted_correspondence_table.append(["**" + str(r) + "**", "**" + d + "**"])
+        else:
+            formatted_correspondence_table.append([str(r), d])
+    st.table(formatted_correspondence_table, headers=["Reminder", "Day of the week"])
