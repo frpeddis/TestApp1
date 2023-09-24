8ba5c40813b441e7c9ab156e67c5d5af6c0fdf48
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-09-24 12:49:16 +0200

    Update chall.py

--- chall.py
+++ chall.py
@@ -1,4 +1,3 @@
-
 import random
 import calendar
 import streamlit as st
@@ -11,86 +10,68 @@ from PIL import Image
 from io import BytesIO
 import pandas as pd
 
-# Checkbox to toggle image display
-openai.api_key = st.secrets["API_KEY"]
-st.set_page_config(page_title="🌀 WeekDay Whiz")
-
-def generate_news(selected_date):
-    prompt = f"What happens on {selected_date}?\nGive me a good news simply with an initial 😄, a neutral news simply with an initial 😐, and a bad news simply with an initial 😔. Do not mention if it is good, neutral or bad news, just use the icons. Do not mention any date in your answer. jump a line forevery news. Insert related Wikipedia links."
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=200,
-        temperature=0.7
-    )
-    return response.choices[0].text.strip()
-
-# Streamlit app title
-st.title(":sunglasses: What day is it? Random date 🎲")
-show_images = st.checkbox("Show me how to calculate ! ")
-
-if show_images:
-    image_links = [
-        "https://raw.githubusercontent.com/frpeddis/TestApp1/1ce97d47cedac010c814496ef6e34773a748cff6/MAGIC%20DAY%20CALCULATOR_1.jpeg",
-        "https://raw.githubusercontent.com/frpeddis/TestApp1/1ce97d47cedac010c814496ef6e34773a748cff6/MAGIC%20DAY%20CALCULATOR_2.jpeg",
-        "https://raw.githubusercontent.com/frpeddis/TestApp1/1ce97d47cedac010c814496ef6e34773a748cff6/MAGIC%20DAY%20CALCULATOR_3.jpeg",
-        "https://raw.githubusercontent.com/frpeddis/TestApp1/1ce97d47cedac010c814496ef6e34773a748cff6/MAGIC%20DAY%20CALCULATOR_4.jpeg"
-    ]
-    for i, link in enumerate(image_links):
-        response = requests.get(link)
-        img = Image.open(BytesIO(response.content))
-        st.image(img, use_column_width=True)
-
 # Function to calculate a random date
 def calculate_random_date():
     start_date = datetime(1582, 10, 15)
     end_date = datetime(2099, 12, 31)
     return start_date + timedelta(seconds=random.randint(0, int((end_date - start_date).total_seconds())))
 
-# Initialize a variable to store the lowest time record, setting it to a high value initially
-lowest_time_record = 1e6  # 1 million seconds as an initial high value
+# Initialize session state variables if not already initialized
+if 'question_count' not in st.session_state:
+    st.session_state.question_count = 0
 
-# Initialize a variable to store the start time
-start_time = datetime.now()
+if 'lowest_time_record' not in st.session_state:
+    st.session_state.lowest_time_record = 1e6  # 1 million seconds as an initial high value
 
-# Initialize a loop to ask questions 5 times
-for i in range(5):
-    # Generate a random date
-    random_date = calculate_random_date()
+if 'start_time' not in st.session_state:
+    st.session_state.start_time = datetime.now()
+
+# If 5 questions are answered, reset the session
+if st.session_state.question_count >= 5:
+    st.session_state.question_count = 0
+    st.session_state.start_time = datetime.now()
+    st.write("Starting a new round of questions.")
+
+# Generate a random date for the current question
+random_date = calculate_random_date()
+
+# Display the random date
+description = "**Random Date:**"
+value = random_date.strftime("%d-%b-%Y")
+st.markdown(f"{description} {value}")
+
+# Prompt the user to select the day of the week from a dropdown list
+selected_day_of_week = st.selectbox(f"Select the day of the week for question {st.session_state.question_count + 1}:", list(calendar.day_name))
+
+# Add a "Check" button to confirm the selection
+check_button = st.button(f"Check for question {st.session_state.question_count + 1}")
+
+# Logic for checking the answer
+if check_button:
+    # Increment the question count
+    st.session_state.question_count += 1
+
+    # Confirm the day of the week selected by the user
+    day_of_week = calendar.day_name[random_date.weekday()]
     
-    # Display the random date
-    description = "**Random Date:**"
-    value = random_date.strftime("%d-%b-%Y")
-    st.markdown(f"{description} {value}")
-    
-    # Prompt the user to select the day of the week from a dropdown list
-    selected_day_of_week = st.selectbox(f"Select the day of the week for question {i+1}:", list(calendar.day_name))
-    
-    # Add a "Check" button to confirm the selection
-    check_button = st.button(f"Check for question {i+1}")
-    
-    # Logic for checking the answer
-    if check_button:
-        # Confirm the day of the week selected by the user
-        day_of_week = calendar.day_name[random_date.weekday()]
-        if selected_day_of_week == day_of_week:
-            st.balloons()
-            st.success(day_of_week + " is OK! :thumbsup:")
-        else:
-            com.iframe("https://lottie.host/?file=380d3ff9-0c30-4a96-b25b-7eeb8868bfeb/vnvhMZFQ8j.json")
-            st.error(day_of_week + " is the right day! :coffee: That's why...")
+    if selected_day_of_week == day_of_week:
+        st.balloons()
+        st.success(day_of_week + " is OK! :thumbsup:")
+    else:
+        com.iframe("https://lottie.host/?file=380d3ff9-0c30-4a96-b25b-7eeb8868bfeb/vnvhMZFQ8j.json")
+        st.error(day_of_week + " is the right day! :coffee: That's why...")
 
-# Calculate the total time taken to complete all 5 questions
-time_taken = (datetime.now() - start_time).total_seconds()
+    # Calculate the total time taken to complete all questions so far
+    time_taken = (datetime.now() - st.session_state.start_time).total_seconds()
+    
+    # Check if the time taken is less than the current lowest time record
+    if time_taken < st.session_state.lowest_time_record:
+        # Update the lowest time record
+        st.session_state.lowest_time_record = time_taken
+        st.write(f"New record! Lowest time taken: {st.session_state.lowest_time_record} seconds")
+    else:
+        st.write(f"Time taken: {time_taken} seconds")
 
-# Check if the time taken is less than the current lowest time record
-if time_taken < lowest_time_record:
-    # Update the lowest time record
-    lowest_time_record = time_taken
-    st.write(f"New record! Lowest time taken: {lowest_time_record} seconds")
-else:
-    st.write(f"Time taken: {time_taken} seconds")
-
-# Display the total time taken and the lowest time record
-st.write(f"Total time taken to complete all 5 questions: {time_taken} seconds")
-st.write(f"Lowest time record: {lowest_time_record} seconds")
+    # Display the total time taken and the lowest time record
+    st.write(f"Total time taken to complete questions so far: {time_taken} seconds")
+    st.write(f"Lowest time record: {st.session_state.lowest_time_record} seconds")
