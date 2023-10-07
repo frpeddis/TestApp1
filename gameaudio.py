721ed24e5ad0ae87606fbf9a09d58aaddddc9b1e
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-10-07 18:03:16 +0200

    Update gameaudio.py

--- gameaudio.py
+++ gameaudio.py
@@ -2,17 +2,18 @@ import random
 import calendar
 import streamlit as st
 from datetime import datetime, timedelta
-import matplotlib.pyplot as plt  # Importing matplotlib for plotting
+import matplotlib.pyplot as plt
 from gtts import gTTS
 from num2words import num2words
-import os
+from io import BytesIO  # Importing BytesIO
 
 # Function to convert text to speech
 def text_to_speech(text):
-    tts = gTTS(text=text, lang='it')  # Language set to Italian
-    filename = 'temp_audio.mp3'
-    tts.save(filename)
-    return filename
+    tts = gTTS(text=text, lang='it')
+    audio_io = BytesIO()
+    tts.save(audio_io, format="mp3")
+    audio_io.seek(0)
+    return audio_io
 
 # Function to convert date to Italian words
 def date_to_italian_words(date):
@@ -37,12 +38,9 @@ def date_to_italian_words(date):
         'November': 'novembre',
         'December': 'dicembre'
     }
-    
+
     month_words = month_map.get(month, '')
     return f"{day_words} {month_words} {year_words}"
-
-
-
 
 # Function to calculate a random date
 def calculate_random_date():
@@ -50,64 +48,43 @@ def calculate_random_date():
     end_date = datetime(2099, 12, 31)
     return start_date + timedelta(seconds=random.randint(0, int((end_date - start_date).total_seconds())))
 
-# Initialize session state variables if not already initialized
+# Initialize session state variables
 if 'question_count' not in st.session_state:
     st.session_state.question_count = 0
-
 if 'total_time' not in st.session_state:
     st.session_state.total_time = 0.0
-
 if 'question_start_time' not in st.session_state:
     st.session_state.question_start_time = datetime.now()
-
 if 'random_date' not in st.session_state:
     st.session_state.random_date = calculate_random_date()
-
 if 'button_label' not in st.session_state:
     st.session_state.button_label = "Check Question 1"
-
 if 'time_list' not in st.session_state:
     st.session_state.time_list = []
-
-if 'show_summary' not in st.session_state:  # New session state to control the summary display
+if 'show_summary' not in st.session_state:
     st.session_state.show_summary = False
 
-
-description = ""
-
-
+# Streamlit app title
 st.title(":sunglasses: What day is it? Random date 🎲")
-
-
-
-
-# Display the random date
-description = "**Random Date:**"
-value = st.session_state.random_date.strftime("%d-%b-%Y")
-#st.markdown(f"{description} {value}")
 
 # Convert the date to Italian words
 date_words = date_to_italian_words(st.session_state.random_date)
 
 # Text to speech
-audio_file_path = text_to_speech(f"{date_words}")
+audio_io = text_to_speech(f"{date_words}")
 
 # Streamlit audio player
-audio_file = open(audio_file_path, 'rb')
-audio_bytes = audio_file.read()
+audio_io.seek(0)  # Ensure the buffer is at the beginning
+audio_bytes = audio_io.read()
 st.audio(audio_bytes, format='audio/mp3')
 
-
-
-
-
-
-# Prompt the user to select the day of the week from a dropdown list
+# User selection for day of the week
 selected_day_of_week = st.selectbox(f"Select the day of the week for question {st.session_state.question_count + 1}:", list(calendar.day_name))
 
-# Add a button to confirm the selection, label changes based on session state
+# Button to confirm the selection
 check_button = st.button(st.session_state.button_label)
 
+# Logic for checking the answer
 # Logic for checking the answer
 if check_button:
     # Confirm the day of the week selected by the user
