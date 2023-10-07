af7a1a58aee083baa7424026b306b40eb233b3c3
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-10-07 11:14:04 +0200

    Update gara1.py

--- gara1.py
+++ gara1.py
@@ -1,8 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import random
 import calendar
 import streamlit as st
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt  # Importing matplotlib for plotting
+from gtts import gTTS
+from num2words import num2words
+import os
+
+# Function to convert text to speech
+def text_to_speech(text):
+    tts = gTTS(text=text, lang='it')  # Language set to Italian
+    filename = 'temp_audio.mp3'
+    tts.save(filename)
+    return filename
+
+# Function to convert date to Italian words
+def date_to_italian_words(date):
+    day = int(date.strftime("%d"))
+    month = date.strftime("%B")
+    year = int(date.strftime("%Y"))
+
+    day_words = num2words(day, lang='it')
+    year_words = num2words(year, lang='it')
+
+    month_map = {
+        'January': 'gennaio',
+        'February': 'febbraio',
+        'March': 'marzo',
+        'April': 'aprile',
+        'May': 'maggio',
+        'June': 'giugno',
+        'July': 'luglio',
+        'August': 'agosto',
+        'September': 'settembre',
+        'October': 'ottobre',
+        'November': 'novembre',
+        'December': 'dicembre'
+    }
+    
+    month_words = month_map.get(month, '')
+    return f"{day_words} {month_words} {year_words}"
+
+
+
 
 # Function to calculate a random date
 def calculate_random_date():
@@ -32,10 +92,35 @@ if 'time_list' not in st.session_state:
 if 'show_summary' not in st.session_state:  # New session state to control the summary display
     st.session_state.show_summary = False
 
+
+description = ""
+
+
+
+
+
+
+
 # Display the random date
 description = "**Random Date:**"
 value = st.session_state.random_date.strftime("%d-%b-%Y")
 st.markdown(f"{description} {value}")
+
+# Convert the date to Italian words
+date_words = date_to_italian_words(st.session_state.random_date)
+
+# Text to speech
+audio_file_path = text_to_speech(f"{date_words}")
+
+# Streamlit audio player
+audio_file = open(audio_file_path, 'rb')
+audio_bytes = audio_file.read()
+st.audio(audio_bytes, format='audio/mp3')
+
+
+
+
+
 
 # Prompt the user to select the day of the week from a dropdown list
 selected_day_of_week = st.selectbox(f"Select the day of the week for question {st.session_state.question_count + 1}:", list(calendar.day_name))
@@ -65,6 +150,9 @@ if check_button:
     
     else:
         st.error(f"{day_of_week} is the right day! :coffee:")
+
+# Cleanup
+os.remove(audio_file_path)
 
 # Actions after 5 questions
 if st.session_state.question_count >= 5:
