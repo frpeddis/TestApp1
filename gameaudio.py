56fe95c1c16ed32abe3702dca5f631fa45db0e5c
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-10-07 15:08:13 +0200

    Update gameaudio.py

--- gameaudio.py
+++ gameaudio.py
@@ -5,25 +5,28 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 from gtts import gTTS
 from num2words import num2words
-import os
-import base64  # Added for base64 encoding
+from io import BytesIO
+from pydub import AudioSegment
 
 # Function to convert text to speech
 def text_to_speech(text):
     tts = gTTS(text=text, lang='it')
-    filename = 'temp_audio.mp3'
-    tts.save(filename)
-    return filename
+    audio_io = BytesIO()
+    tts.save(audio_io, format='mp3')
+    audio_io.seek(0)
+    audio = AudioSegment.from_file(audio_io, format="mp3")
+    audio_io.seek(0)
+    audio.export(audio_io, format="mp3")
+    return audio_io
 
 # Function to convert date to Italian words
+# (This part remains unchanged from your previous code)
 def date_to_italian_words(date):
     day = int(date.strftime("%d"))
     month = date.strftime("%B")
     year = int(date.strftime("%Y"))
-
     day_words = num2words(day, lang='it')
     year_words = num2words(year, lang='it')
-
     month_map = {
         'January': 'gennaio',
         'February': 'febbraio',
@@ -38,17 +41,18 @@ def date_to_italian_words(date):
         'November': 'novembre',
         'December': 'dicembre'
     }
-    
     month_words = month_map.get(month, '')
     return f"{day_words} {month_words} {year_words}"
 
 # Function to calculate a random date
+# (This part remains unchanged from your previous code)
 def calculate_random_date():
     start_date = datetime(1582, 10, 15)
     end_date = datetime(2099, 12, 31)
     return start_date + timedelta(seconds=random.randint(0, int((end_date - start_date).total_seconds())))
 
 # Initialize session state variables
+# (This part remains unchanged from your previous code)
 if 'question_count' not in st.session_state:
     st.session_state.question_count = 0
 if 'total_time' not in st.session_state:
@@ -64,58 +68,71 @@ if 'time_list' not in st.session_state:
 if 'show_summary' not in st.session_state:
     st.session_state.show_summary = False
 
+# Streamlit app title
 st.title(":sunglasses: What day is it? Random date 🎲")
 
-description = "**Random Date:**"
-value = st.session_state.random_date.strftime("%d-%b-%Y")
-
-# Convert date to Italian words
+# Convert the date to Italian words
 date_words = date_to_italian_words(st.session_state.random_date)
 
 # Text to speech
 audio_file_path = text_to_speech(f"{date_words}")
 
-# Read audio file to bytes
-audio_file = open(audio_file_path, 'rb')
-audio_bytes = audio_file.read()
+# Streamlit audio player
+audio_bytes = audio_file_path.read()
+st.audio(audio_bytes, format='audio/mp3')
 
-# Encode as base64
-audio_base64 = base64.b64encode(audio_bytes).decode()
-
-# Embed as HTML audio tag
-audio_html = f'<audio controls><source src="data:audio/mp3;base64,{audio_base64}" type="audio/mpeg"></audio>'
-st.markdown(audio_html, unsafe_allow_html=True)
-
-# Prompt the user to select the day of the week from a dropdown list
+# User selection for day of the week
 selected_day_of_week = st.selectbox(f"Select the day of the week for question {st.session_state.question_count + 1}:", list(calendar.day_name))
 
-# Add a button to confirm the selection, label changes based on session state
+# Button to confirm the selection
 check_button = st.button(st.session_state.button_label)
 
 # Logic for checking the answer
 if check_button:
-    # Confirm the day of the week selected by the user
     day_of_week = calendar.day_name[st.session_state.random_date.weekday()]
-    
     if selected_day_of_week == day_of_week:
         st.balloons()
-        st.markdown(f"{description} {value}")
         st.success(f"{day_of_week} is OK! :thumbsup:")
 
-        # Calculate the time taken for this question
+        # Time calculation
         question_time_taken = (datetime.now() - st.session_state.question_start_time).total_seconds()
         st.session_state.total_time += question_time_taken
         st.session_state.time_list.append(question_time_taken)
 
-        # Update for the next question
+        # Prepare for the next question
         st.session_state.question_count += 1
         st.session_state.question_start_time = datetime.now()
         st.session_state.random_date = calculate_random_date()
         st.session_state.button_label = f"Check Question {st.session_state.question_count + 1}"
-    
     else:
-        st.markdown(f"{description} {value}")
         st.error(f"{day_of_week} is the right day! :coffee:")
 
-# Cleanup
-os.remove(audio_file_path)
+# Show summary after 5 questions
+if st.session_state.question_count >= 5:
+    st.session_state.show_summary = True
+
+if st.session_state.show_summary:
+    average_time = st.session_state.total_time / 5
+    st.write(f"Total time taken for all 5 questions: {round(st.session_state.total_time, 2)} seconds")
+    st.write(f"Shortest time taken: {round(min(st.session_state.time_list), 2)} seconds")
+    st.write(f"Average time taken: {round(average_time, 2)} seconds")
+    st.write(f"Longest time taken: {round(max(st.session_state.time_list), 2)} seconds")
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, 6), st.session_state.time_list, marker='o', linestyle='--')
+    plt.axhline(y=average_time, color='r', linestyle='-')
+    plt.xlabel('Question Number')
+    plt.ylabel('Time Taken (s)')
+    plt.xticks(range(1, 6))
+    plt.ylim(bottom=0)
+    plt.title('Time Taken for Each Question')
+    plt.legend(['Time Taken', 'Average Time'])
+    st.pyplot(plt)
+
+    if st.button("Restart"):
+        st.session_state.question_count = 0
+        st.session_state.total_time = 0.0
+        st.session_state.time_list = []
+        st.session_state.button_label = "Check Question 1"
+        st.session_state.show_summary = False
+        st.experimental_rerun()
