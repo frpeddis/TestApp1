559b063f2ecb9715ff37382964cd2480ae6c74bb
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-10-14 15:30:23 +0200

    Update troppoforte.py

--- troppoforte.py
+++ troppoforte.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 from gtts import gTTS
 from num2words import num2words
 from io import BytesIO
-import wave
 
 # Function to convert text to speech
 def text_to_speech(text):
@@ -54,23 +53,29 @@ def calculate_random_date():
     )
 
 # Initialize session state variables
+if 'error_count_list' not in st.session_state:
+    st.session_state.error_count_list = [0] * 5
+
 if 'question_count' not in st.session_state:
     st.session_state.question_count = 0
+
 if 'total_time' not in st.session_state:
     st.session_state.total_time = 0.0
+
 if 'question_start_time' not in st.session_state:
     st.session_state.question_start_time = datetime.now()
+
 if 'random_date' not in st.session_state:
     st.session_state.random_date = calculate_random_date()
+
 if 'button_label' not in st.session_state:
     st.session_state.button_label = "Check Question 1"
+
 if 'time_list' not in st.session_state:
     st.session_state.time_list = []
+
 if 'show_summary' not in st.session_state:
     st.session_state.show_summary = False
-# Initialize error list
-if 'error_list' not in st.session_state:
-    st.session_state.error_list = []
 
 # Streamlit app title
 st.title(":sunglasses: What day is it? Random date 🎲")
@@ -101,21 +106,20 @@ if check_button:
     if selected_day_of_week == day_of_week:
         st.balloons()
         st.success(f"{day_of_week} is OK! :thumbsup:")
-        st.session_state.error_list.append(0)
+        
+        question_time_taken = (
+            datetime.now() - st.session_state.question_start_time
+        ).total_seconds()
+        st.session_state.total_time += question_time_taken
+        st.session_state.time_list.append(question_time_taken)
+        
+        st.session_state.question_count += 1
+        st.session_state.question_start_time = datetime.now()
+        st.session_state.random_date = calculate_random_date()
+        st.session_state.button_label = f"Check Question {st.session_state.question_count + 1}"
     else:
         st.error(f"{day_of_week} is the right day! :coffee:")
-        st.session_state.error_list.append(1)
-    
-    question_time_taken = (
-        datetime.now() - st.session_state.question_start_time
-    ).total_seconds()
-    st.session_state.total_time += question_time_taken
-    st.session_state.time_list.append(question_time_taken)
-    
-    st.session_state.question_count += 1
-    st.session_state.question_start_time = datetime.now()
-    st.session_state.random_date = calculate_random_date()
-    st.session_state.button_label = f"Check Question {st.session_state.question_count + 1}"
+        st.session_state.error_count_list[st.session_state.question_count] += 1
 
 # Show summary after 5 questions
 if st.session_state.question_count >= 5:
@@ -125,29 +129,24 @@ if st.session_state.show_summary:
     average_time = st.session_state.total_time / 5
     st.write(f"Total time taken for all 5 questions: {round(st.session_state.total_time, 2)} seconds")
     st.write(f"Shortest time taken: {round(min(st.session_state.time_list), 2)} seconds")
+    st.markdown(f'<p style="color:red;">Average time taken: {round(average_time, 2)} seconds</p>', unsafe_allow_html=True)
     st.write(f"Longest time taken: {round(max(st.session_state.time_list), 2)} seconds")
-    
-    for error in st.session_state.error_list:
-        color = "green" if error == 0 else "red"
-        st.markdown(f'<span style="color:{color}; font-size:50px;">•</span>', unsafe_allow_html=True)
-    
+
     plt.figure(figsize=(10, 6))
-    color_map = ['green' if x == 0 else 'red' for x in st.session_state.error_list]
-    plt.scatter(range(1, 6), st.session_state.time_list, c=color_map, marker='o')
+    plt.plot(range(1, 6), st.session_state.time_list, marker='o', linestyle='--')
+    
+    for i, error_count in enumerate(st.session_state.error_count_list):
+        color = 'g' if error_count == 0 else 'r'
+        plt.scatter(i+1, st.session_state.time_list[i], color=color)
+
     plt.axhline(y=average_time, color='r', linestyle='-')
     plt.xlabel('Question Number')
     plt.ylabel('Time Taken (s)')
     plt.xticks(range(1, 6))
     plt.ylim(bottom=0)
     plt.title('Time Taken for Each Question')
-    plt.legend(['Average Time'])
+    plt.legend(['Time Taken', 'Average Time'])
     st.pyplot(plt)
 
-    if st.button("Restart"):
-        st.session_state.question_count = 0
-        st.session_state.total_time = 0.0
-        st.session_state.time_list = []
-        st.session_state.error_list = []
-        st.session_state.button_label = "Check Question 1"
-        st.session_state.show_summary = False
-        st.experimental_rerun()
+    # Add a button to restart a new session
+    if st.button("
