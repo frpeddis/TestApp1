5cca1bb252faebd998a87261344c2e1accdc646e
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-10-14 15:41:55 +0200

    Update troppoforte.py

--- troppoforte.py
+++ troppoforte.py
@@ -15,7 +15,6 @@ def text_to_speech(text):
         tts.save(temp.name)
         temp.seek(0)
         audio_data = temp.read()
-
     audio_io = BytesIO(audio_data)
     audio_io.seek(0)
     return audio_io
@@ -53,27 +52,20 @@ def calculate_random_date():
     )
 
 # Initialize session state variables
-if 'error_count_list' not in st.session_state:
-    st.session_state.error_count_list = [0] * 5
-
 if 'question_count' not in st.session_state:
     st.session_state.question_count = 0
-
+if 'error_count_list' not in st.session_state:
+    st.session_state.error_count_list = [0] * 5
 if 'total_time' not in st.session_state:
     st.session_state.total_time = 0.0
-
 if 'question_start_time' not in st.session_state:
     st.session_state.question_start_time = datetime.now()
-
 if 'random_date' not in st.session_state:
     st.session_state.random_date = calculate_random_date()
-
 if 'button_label' not in st.session_state:
     st.session_state.button_label = "Check Question 1"
-
 if 'time_list' not in st.session_state:
     st.session_state.time_list = []
-
 if 'show_summary' not in st.session_state:
     st.session_state.show_summary = False
 
@@ -106,20 +98,19 @@ if check_button:
     if selected_day_of_week == day_of_week:
         st.balloons()
         st.success(f"{day_of_week} is OK! :thumbsup:")
-        
-        question_time_taken = (
-            datetime.now() - st.session_state.question_start_time
-        ).total_seconds()
-        st.session_state.total_time += question_time_taken
-        st.session_state.time_list.append(question_time_taken)
-        
-        st.session_state.question_count += 1
-        st.session_state.question_start_time = datetime.now()
-        st.session_state.random_date = calculate_random_date()
-        st.session_state.button_label = f"Check Question {st.session_state.question_count + 1}"
     else:
-        st.error(f"{day_of_week} is the right day! :coffee:")
         st.session_state.error_count_list[st.session_state.question_count] += 1
+        st.error(f"{day_of_week} is the right day! :coffee:")
+
+    question_time_taken = (
+        datetime.now() - st.session_state.question_start_time
+    ).total_seconds()
+    st.session_state.total_time += question_time_taken
+    st.session_state.time_list.append(question_time_taken)
+    st.session_state.question_count += 1
+    st.session_state.question_start_time = datetime.now()
+    st.session_state.random_date = calculate_random_date()
+    st.session_state.button_label = f"Check Question {st.session_state.question_count + 1}"
 
 # Show summary after 5 questions
 if st.session_state.question_count >= 5:
@@ -129,7 +120,6 @@ if st.session_state.show_summary:
     average_time = st.session_state.total_time / 5
     st.write(f"Total time taken for all 5 questions: {round(st.session_state.total_time, 2)} seconds")
     st.write(f"Shortest time taken: {round(min(st.session_state.time_list), 2)} seconds")
-    st.markdown(f'<p style="color:red;">Average time taken: {round(average_time, 2)} seconds</p>', unsafe_allow_html=True)
     st.write(f"Longest time taken: {round(max(st.session_state.time_list), 2)} seconds")
 
     plt.figure(figsize=(10, 6))
@@ -148,7 +138,6 @@ if st.session_state.show_summary:
     plt.legend(['Time Taken', 'Average Time'])
     st.pyplot(plt)
 
-    # Add a button to restart a new session
     if st.button("Restart"):
         st.session_state.show_summary = False
         st.session_state.question_count = 0
@@ -157,4 +146,3 @@ if st.session_state.show_summary:
         st.session_state.error_count_list = [0] * 5
         st.session_state.random_date = calculate_random_date()
         st.session_state.button_label = "Check Question 1"
-
