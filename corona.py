de14f5a0bd9b72dc746d1cd3dfe40c5ee34064d3
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-08 20:04:12 +0100

    Update corona.py

--- corona.py
+++ corona.py
@@ -12,14 +12,35 @@ def create_pie_chart(selected_day):
 
     return fig
 
-# Streamlit UI
+# Streamlit UI for selecting a day
 st.title('Weekday Selector')
+
+# Creating a row of buttons
+col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 selected_day = None
 
-# Buttons for each day
-for day in ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']:
-    if st.button(day):
-        selected_day = day
+with col1:
+    if st.button(days[0]):
+        selected_day = days[0]
+with col2:
+    if st.button(days[1]):
+        selected_day = days[1]
+with col3:
+    if st.button(days[2]):
+        selected_day = days[2]
+with col4:
+    if st.button(days[3]):
+        selected_day = days[3]
+with col5:
+    if st.button(days[4]):
+        selected_day = days[4]
+with col6:
+    if st.button(days[5]):
+        selected_day = days[5]
+with col7:
+    if st.button(days[6]):
+        selected_day = days[6]
 
 # Display the pie chart
 fig = create_pie_chart(selected_day)
