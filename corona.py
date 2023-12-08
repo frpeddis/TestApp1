fe338b3273e501666a0eb7292d754a2c7e8f654f
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-08 19:59:36 +0100

    Update corona.py

--- corona.py
+++ corona.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import plotly.graph_objects as go
 
-# Funzione per creare il grafico a torta
+# Function to create the pie chart
 def create_pie_chart(selected_day):
     days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     colors = ['blue' if day == selected_day else 'lightgray' for day in days]
@@ -12,33 +12,15 @@ def create_pie_chart(selected_day):
 
     return fig
 
-# Inizializzazione della variabile selezionata
-selected_day = None
-
 # Streamlit UI
 st.title('Weekday Selector')
+selected_day = None
 
-# Visualizzazione del grafico a torta
+# Buttons for each day
+for day in ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']:
+    if st.button(day):
+        selected_day = day
+
+# Display the pie chart
 fig = create_pie_chart(selected_day)
-pie_chart = st.plotly_chart(fig, use_container_width=True)
-
-# Gestione del clic sul grafico
-@st.cache(allow_output_mutation=True)
-def get_click_data():
-    return {'day': None}
-
-click_data = get_click_data()
-
-# Rilevamento clic e aggiornamento grafico
-clicked = pie_chart.clicked_points
-if clicked and isinstance(clicked, list):
-    # Assicurati che ci sia almeno un elemento in 'clicked' e che contenga 'label'
-    if len(clicked) > 0 and isinstance(clicked[0], dict) and 'label' in clicked[0]:
-        selected_day = clicked[0]['label']
-        click_data['day'] = selected_day
-        fig = create_pie_chart(selected_day)
-        pie_chart.plotly_chart(fig, use_container_width=True)
-
-# Visualizzazione del giorno selezionato
-if click_data['day']:
-    st.write(f"You selected: {click_data['day']}")
+st.plotly_chart(fig, use_container_width=True)
