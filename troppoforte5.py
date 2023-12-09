266ef9f1897b94a99114d224baac9eabf099934a
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-09 22:23:10 +0100

    Update troppoforte5.py

--- troppoforte5.py
+++ troppoforte5.py
@@ -28,7 +28,8 @@ def text_to_speech(text, random_date):
     audio_io = BytesIO(audio_data)
     audio_io.seek(0)
     return audio_io
-    # Funzione per convertire la data in parole italiane
+
+# Funzione per convertire la data in parole italiane
 def date_to_italian_words(date):
     day = int(date.strftime("%d"))
     month = date.strftime("%B")
@@ -94,6 +95,17 @@ audio_io.seek(0)
 audio_bytes = audio_io.read()
 st.audio(audio_bytes, format='audio/wav')
 
+# Creating two columns for the layout
+left_column, right_column = st.beta_columns(2)
+
+# In the left column, place the day selection radio buttons
+with left_column:
+    day_options = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    selected_day = st.radio("Seleziona un giorno della settimana:", day_options)
+
+    # Button to confirm the selection and check the answer
+    check_button = st.button(st.session_state.button_label)
+
 # Function to create the pie chart
 def create_pie_chart(selected_day, correct_day=None):
     days_short = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
@@ -110,68 +122,11 @@ def create_pie_chart(selected_day, correct_day=None):
 
     return fig
 
-# User selection for day of the week (with radio buttons)
-day_options = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-selected_day = st.radio("Seleziona un giorno della settimana:", day_options)
+# In the right column, display the pie chart
+with right_column:
+    if check_button and selected_day:
+        day_of_week = calendar.day_name[st.session_state.random_date.weekday()]
+        fig = create_pie_chart(selected_day, day_of_week)
+        st.plotly_chart(fig, use_container_width=True)
 
-# Button to confirm the selection and check the answer
-check_button = st.button(st.session_state.button_label)
-
-# Logic for checking the answer and updating the pie chart
-if check_button and selected_day:
-    day_of_week = calendar.day_name[st.session_state.random_date.weekday()]
-    fig = create_pie_chart(selected_day, day_of_week)
-    st.plotly_chart(fig, use_container_width=True)
-
-    if selected_day == day_of_week:
-        st.balloons()
-        st.success(f"{day_of_week} è corretto! :thumbsup:")
-    else:
-        st.session_state.error_count_list[st.session_state.question_count] += 1
-        st.error(f"{day_of_week} è il giorno giusto! :coffee:")
-
-    question_time_taken = (
-        datetime.now() - st.session_state.question_start_time
-    ).total_seconds()
-    st.session_state.total_time += question_time_taken
-    st.session_state.time_list.append(question_time_taken)
-    st.session_state.question_count += 1
-    st.session_state.question_start_time = datetime.now()
-    st.session_state.random_date = calculate_random_date()
-    st.session_state.button_label = f"Check Question {st.session_state.question_count + 1} / NEXT"
-
-# Show summary after 5 questions
-if st.session_state.question_count >= 5:
-    st.session_state.show_summary = True
-
-if st.session_state.show_summary:
-    average_time = st.session_state.total_time / 5
-    st.write(f"Total time taken for all 5 questions: {round(st.session_state.total_time, 2)} seconds")
-    st.write(f"Shortest time taken: {round(min(st.session_state.time_list), 2)} seconds")
-    st.markdown(f'<p style="color:fuchsia;">Average time taken: {round(average_time, 2)} seconds</p>', unsafe_allow_html=True)
-    st.write(f"Longest time taken: {round(max(st.session_state.time_list), 2)} seconds")
-
-    plt.figure(figsize=(10, 6))
-    plt.plot(range(1, 6), st.session_state.time_list, marker='o', linestyle='--', label='Time Taken')
-    
-    for i, (time_taken, error_count) in enumerate(zip(st.session_state.time_list, st.session_state.error_count_list)):
-        color = 'g' if error_count == 0 else 'r'
-        plt.scatter(i+1, time_taken, color=color, zorder=5, s=100, label=None)
-    
-    plt.axhline(y=average_time, color='fuchsia', linestyle='-', label='Average Time')
-    plt.xlabel('Question Number')
-    plt.ylabel('Time Taken (s)')
-    plt.xticks(range(1, 6))
-    plt.ylim(bottom=0)
-    plt.title('Time Taken for Each Question')
-    plt.legend()
-    st.pyplot(plt)
-
-    
-    if st.button("Restart"):
-        st.session_state.question_count = 0
-        st.session_state.total_time = 0.0
-        st.session_state.time_list = []
-        st.session_state.button_label = "Check Question 1"
-        st.session_state.show_summary = False
-        st.experimental_rerun()
+# ... [rest of your code for summary and restart button]
