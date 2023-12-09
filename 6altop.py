55678bb97609b3432835ca4d7edfbc9bfe6173a0
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-09 23:56:02 +0100

    Update 6altop.py

--- 6altop.py
+++ 6altop.py
@@ -15,7 +15,7 @@ def text_to_speech(text, random_date):
     prefix = "Che giorno era il " if random_date < today - timedelta(days=1) else "Che giorno sarà il "
     
     # Randomly select a different language
-    languages = ['it', 'fr', 'es', 'pt']
+    languages = ['it', 'fr', 'es', 'ro']
     selected_lang = random.choice(languages)
     
     tts = gTTS(text=f"{prefix} {text}", lang=selected_lang)
