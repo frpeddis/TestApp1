e15cc68bab7ae669e3271a2af714b8a3ed231795
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-10 18:23:36 +0100

    Update troppoforte.py

--- troppoforte.py
+++ troppoforte.py
@@ -153,15 +153,11 @@ with right_column:
         st.session_state.random_date = calculate_random_date()
         st.session_state.button_label = f"Check number {st.session_state.question_count + 1} / NEXT"
 
-# Calculate success percentage
-success_percentage = (5 - sum(st.session_state.error_count_list)) / 5 * 100 if st.session_state.question_count >= 5 else 0
-
-# Show success percentage in the recap only after the 5th question
-if st.session_state.question_count >= 5:
-    st.write(f"Success percentage: {success_percentage:.2f}%")
-
 # Show summary after 5 questions
 if st.session_state.question_count >= 5:
+    st.session_state.show_summary = True
+
+if st.session_state.show_summary:
     
     average_time = st.session_state.total_time / 5
     st.write(f"Total time taken for all 5 questions: {round(st.session_state.total_time, 2)} seconds")
