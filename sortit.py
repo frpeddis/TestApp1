b301a930e50d2efe73b128a68d253a28d5c4f432
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-26 17:52:44 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -10,23 +10,23 @@ if uploaded_file is not None:
     try:
         # Leggi il file CSV caricato
         data = pd.read_csv(uploaded_file)
-        st.session_state['data'] = data  # Memorizza i dati nella sessione
     except Exception as e:
         st.error(f"Errore nella lettura del file: {e}")
-        st.session_state['data'] = pd.DataFrame()
+        data = pd.DataFrame()
 else:
     st.info("Attendi il caricamento del file CSV.")
-    st.session_state['data'] = pd.DataFrame()
+    data = pd.DataFrame()
 
 # Verifica se i dati sono sufficienti
-if st.session_state.get('data', pd.DataFrame()).empty:
+if data.empty:
     st.error("Il DataFrame è vuoto.")
-elif len(st.session_state['data']) < 5:
+elif len(data) < 5:
     st.error("Non ci sono abbastanza dati per selezionare 5 record casuali.")
 else:
     # Seleziona 5 record casuali, solo se non sono già stati selezionati
     if 'selected_records' not in st.session_state:
-        st.session_state['selected_records'] = st.session_state['data'].sample(5)
+        st.session_state['selected_records'] = data.sample(5)
+        st.session_state['ordered_names'] = []
 
     # Container 1: Descrizione Sintetica
     with st.container():
@@ -37,28 +37,28 @@ else:
     # Container 2: Ordinamento Eventi
     with st.container():
         st.subheader("Ordina gli Eventi per Data")
-        ordered_names = [row['Descrizione Breve'] for _, row in st.session_state['selected_records'].iterrows()]
-        
-        # Implementazione di riordinamento
-        item_to_move = st.selectbox("Seleziona un evento da spostare:", ordered_names)
-        new_position = st.selectbox("Scegli la nuova posizione:", range(len(ordered_names)))
-        
-        if st.button("Conferma Spostamento"):
-            # Sposta l'elemento nella nuova posizione
-            ordered_names.remove(item_to_move)
-            ordered_names.insert(new_position, item_to_move)
-            st.session_state['selected_records'] = st.session_state['selected_records'].loc[st.session_state['selected_records']['Descrizione Breve'].isin(ordered_names)]
+
+        # Selezione degli eventi da ordinare
+        all_names = [row['Descrizione Breve'] for _, row in st.session_state['selected_records'].iterrows()]
+        selected_name = st.selectbox("Scegli un evento da aggiungere all'ordine:", all_names)
+        if st.button("Aggiungi all'Ordine"):
+            if selected_name not in st.session_state['ordered_names']:
+                st.session_state['ordered_names'].append(selected_name)
+
+        # Mostra gli eventi selezionati in ordine
+        for name in st.session_state['ordered_names']:
+            st.write(name)
 
         # Controlla l'ordine
         if st.button("Verifica Ordine"):
             ordered_records = pd.DataFrame()
-            for name in ordered_names:
+            for name in st.session_state['ordered_names']:
                 ordered_records = ordered_records.append(st.session_state['selected_records'][st.session_state['selected_records']['Descrizione Breve'] == name])
 
             ordered_correctly = ordered_records['anno della scoperta'].is_monotonic_increasing
-            if ordered_correctly and len(ordered_records) == 5:
+            if ordered_correctly and len(ordered_records) == len(st.session_state['ordered_names']):
                 st.success("Hai indovinato l'ordine corretto!")
                 for _, row in ordered_records.iterrows():
-                    st.write(f"{row['Descrizione Breve']} - {row['anno della scoperta']} - {row['descrizione estesa']} - {row['Nome inventore']} - {row['Paese di origine']}")
+                    st.write(f"{row['Descrizione Breve']} - {row['anno della scoperta']}")
             else:
                 st.error("Ordine non corretto. Riprova.")
