7bc73ac39069798e1234810c8728da01067d7560
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-26 19:20:22 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -3,7 +3,7 @@ import pandas as pd
 from streamlit_sortables import sort_items
 
 # Titolo dell'applicazione
-st.title('Scoperte Invenzioni')
+st.title('Quanto sei forte ?')
 
 # Carica il file CSV
 uploaded_file = st.file_uploader("Carica un file CSV", type="csv")
@@ -19,14 +19,14 @@ if not data.empty and len(data) >= 5:
         st.session_state['selected_records'] = data.sample(5)
 
     # Mostra le invenzioni casuali
-    st.write('Invenzioni Casuali:')
+    st.write('Metti in ordine queste invenzioni:')
     items = [{'header': 'Invenzioni', 'items': list(st.session_state['selected_records']['Descrizione Breve'])}]
 
     # Utilizza streamlit-sortables per ordinare gli elementi
     sorted_items = sort_items(items, multi_containers=True, direction="vertical")
 
     # Verifica l'ordine
-    if st.button("Verifica Ordine"):
+    if st.button("Ce l'hai fatta ?"):
         ordered_records = pd.DataFrame()
         for desc in sorted_items[0]['items']:
             matching_record = st.session_state['selected_records'][st.session_state['selected_records']['Descrizione Breve'] == desc]
@@ -39,10 +39,10 @@ if not data.empty and len(data) >= 5:
         if ordered_correctly and len(ordered_records) == len(sorted_items[0]['items']):
             st.balloons()
             st.markdown("<div style='background-color:blue;color:white;padding:14px;'>"
-                        "Hai indovinato l'ordine corretto!</div>", unsafe_allow_html=True)
+                        "Daje !!! Hai indovinato l'ordine corretto!</div>", unsafe_allow_html=True)
             for _, row in ordered_records.iterrows():
                 st.markdown(f"<div style='background-color:darkblue;color:white;padding:12px;'>"
                             f"{row['Descrizione Breve']} - {row['Anno di Scoperta']} - {row['Nome Inventore']} - {row['Paese']} - {row['Descrizione Lunga']}</div>",
                             unsafe_allow_html=True)
         else:
-            st.error("Ordine non corretto. Riprova.")
+            st.error("Mannaggia, l'ordine non è corretto. Riprova.")
