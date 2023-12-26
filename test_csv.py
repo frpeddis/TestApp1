652db84dd9f9c12d024db643f4adfe089d6dfdbf
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-26 17:10:43 +0100

    Update test_csv.py

--- test_csv.py
+++ test_csv.py
@@ -1,9 +1,23 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-st.title("Hello world!")
+# Title of the app
+st.title('CSV File Upload and Display')
 
-uploaded_file = st.file_uploader("Choose a file")
+# Instruction
+st.markdown('**Please upload a CSV file to view its content.**')
+
+# File uploader allows user to add their own CSV
+uploaded_file = st.file_uploader("Upload CSV", type=["csv"])
+
+# Check if there is a file uploaded
 if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  st.write(dataframe)
+    # Read the CSV file
+    df = pd.read_csv(uploaded_file)
+    
+    # Show the content of the CSV file
+    st.write(df)
+
+# If no file is uploaded, show a message
+else:
+    st.write("Please upload a CSV file to proceed.")
