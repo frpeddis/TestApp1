9372bd0729cdea71835eb09157e65bb49a82ea36
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-26 19:35:06 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -26,9 +26,10 @@ if not data.empty and len(data) >= 5:
         st.session_state['selected_records'] = data.sample(5)
 
     # Mostra le invenzioni casuali
-    #st.write('Metti in ordine queste invenzioni:')
-    items = [{'header': 'Metti in ordine queste invenzioni! In alto le più antiche!', 'items': list(st.session_state['selected_records']['Descrizione Breve'])}]
-
+    
+    items = [{'header': 'Metti in ordine queste invenzioni!', 'items': list(st.session_state['selected_records']['Descrizione Breve'])}]
+    st.write('In alto le più antiche!')
+    
     # Utilizza streamlit-sortables per ordinare gli elementi
     sorted_items = sort_items(items, multi_containers=True, direction="vertical")
 
