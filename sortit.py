e17202e4f0043bf64191283ebe334542869e625e
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-26 19:17:37 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -38,11 +38,11 @@ if not data.empty and len(data) >= 5:
         ordered_correctly = ordered_records['Anno di Scoperta'].is_monotonic_increasing
         if ordered_correctly and len(ordered_records) == len(sorted_items[0]['items']):
             st.balloons()
-            st.markdown("<div style='background-color:lightgreen;color:white;padding:14px;'>"
+            st.markdown("<div style='background-color:blue;color:white;padding:14px;'>"
                         "Hai indovinato l'ordine corretto!</div>", unsafe_allow_html=True)
             for _, row in ordered_records.iterrows():
                 st.markdown(f"<div style='background-color:darkblue;color:white;padding:12px;'>"
-                            f"{row['Descrizione Breve']} - {row['Anno di Scoperta']} - {row['Descrizione Lunga']}</div>",
+                            f"{row['Descrizione Breve']} - {row['Anno di Scoperta']} - {row['Nome Inventore']} - {row['Paese']} - {row['Descrizione Lunga']}</div>",
                             unsafe_allow_html=True)
         else:
             st.error("Ordine non corretto. Riprova.")
