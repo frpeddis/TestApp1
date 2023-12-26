2fc99031eb7df814ab7f7185c9e5fc0bc717d6d8
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-26 16:03:11 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -1,8 +1,8 @@
+
 import streamlit as st
 import pandas as pd
 import random
 import requests
-#from streamlit_lottie import st_lottie
 
 # Funzione per caricare i dati da GitHub
 @st.cache
@@ -19,43 +19,45 @@ def load_data_from_github(url):
 csv_url = 'https://raw.githubusercontent.com/your_username/TestApp1/main/Invenzioni.csv'
 data = load_data_from_github(csv_url)
 
-# Seleziona 5 record casuali
-selected_records = data.sample(5)
 
-# Funzione per caricare un'animazione Lottie
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+# Verifica se i dati sono sufficienti
+if data.empty or len(data) < 5:
+    st.error("I dati non sono sufficienti per la selezione casuale.")
+else:
+    # Seleziona 5 record casuali
+    selected_records = data.sample(5)
 
-# Suono di vittoria (modifica con un URL appropriato per un'animazione Lottie)
-victory_sound = load_lottieurl('your_lottie_sound_animation_url')
+    # Layout Streamlit
+    st.title("Scoperte Invenzioni")
 
-# Layout Streamlit
-st.title("Scoperte Invenzioni")
-
-# Container 1: Descrizione Sintetica
-with st.container():
-    st.subheader("Descrizioni Sintetiche")
-    for _, row in selected_records.iterrows():
-        st.write(f"- {row['nome sintetico di invenzione']}")
-
-# Container 2: Ordinamento Eventi
-with st.container():
-    st.subheader("Ordina gli Eventi per Data")
-    ordered_names = st.columns(5)
-    for i, name in enumerate(selected_records['nome sintetico di invenzione']):
-        ordered_names[i].write(name)
-
-# Controlla l'ordine
-if st.button("Verifica Ordine"):
-    ordered_correctly = True # Aggiungi la logica di controllo qui
-    if ordered_correctly:
-        st.success("Hai indovinato l'ordine corretto!")
-        #st_lottie(victory_sound, height=300, key="win")
-        # Mostra dettagli aggiuntivi
+    # Container 1: Descrizione Sintetica
+    with st.container():
+        st.subheader("Descrizioni Sintetiche")
         for _, row in selected_records.iterrows():
-            st.write(f"{row['nome sintetico di invenzione']} - {row['anno della scoperta']} - {row['descrizione estesa']} - {row['Nome inventore']} - {row['Paese di origine']}")
-    else:
-        st.error("Ordine non corretto. Riprova.")
+            st.write(f"- {row['nome sintetico di invenzione']}")
+
+    # Container 2: Ordinamento Eventi
+    with st.container():
+        st.subheader("Ordina gli Eventi per Data")
+
+        # Crea una lista modificabile di nomi
+        ordered_names = [row['nome sintetico di invenzione'] for _, row in selected_records.iterrows()]
+        ordered_names = st.multiselect("Riordina gli eventi qui:", ordered_names, ordered_names)
+
+        # Controlla l'ordine
+        if st.button("Verifica Ordine"):
+            # Estrai i record nell'ordine selezionato
+            ordered_records = pd.DataFrame()
+            for name in ordered_names:
+                ordered_records = ordered_records.append(selected_records[selected_records['nome sintetico di invenzione'] == name])
+
+            # Verifica se l'ordine per anno è corretto
+            ordered_correctly = ordered_records['anno della scoperta'].is_monotonic_increasing
+
+            if ordered_correctly and len(ordered_records) == 5:
+                st.success("Hai indovinato l'ordine corretto!")
+                # Mostra dettagli aggiuntivi
+                for _, row in ordered_records.iterrows():
+                    st.write(f"{row['nome sintetico di invenzione']} - {row['anno della scoperta']} - {row['descrizione estesa']} - {row['Nome inventore']} - {row['Paese di origine']}")
+            else:
+                st.error("Ordine non corretto. Riprova.")
