e6b94f876ad663bec376744b7d9b9460a5e7b8f9
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-26 16:32:24 +0100

    Update test_csv.py

--- test_csv.py
+++ test_csv.py
@@ -2,9 +2,9 @@ DATA_URL = ('https://raw.githubusercontent.com/frpeddis/TestApp1/main/Invenzioni
 @st.cache
 def load_data():
     data = pd.read_csv(DATA_URL)
-    data['Date'] = pd.to_datetime(data['Date']).dt.strftime('%Y-%m-%d')
+
     return data
 df = load_data()
 
 # show data on streamlit
- st.write(df)
+st.write(df)
