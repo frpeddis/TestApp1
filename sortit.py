cb3042dae129161c2db06ffc9ec088362d163295
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-26 18:41:21 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -9,8 +9,6 @@ st.title('Scoperte Invenzioni')
 uploaded_file = st.file_uploader("Carica un file CSV", type="csv")
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
-    # Stampare i nomi delle colonne per il debugging
-    st.write('Nomi delle colonne nel file CSV:', data.columns.tolist())
 else:
     st.info("Attendi il caricamento del file CSV.")
     data = pd.DataFrame()
@@ -33,21 +31,17 @@ if not data.empty and len(data) >= 5:
         for desc in sorted_items[0]['items']:
             matching_record = st.session_state['selected_records'][st.session_state['selected_records']['Descrizione Breve'] == desc]
             if not matching_record.empty:
-                ordered_records = pd.concat([ordered_records, matching_record], ignore_index=True)
+                ordered_records = pd.concat([ordered_records, matching_record])
             else:
                 st.error(f"L'elemento '{desc}' non trovato nei record selezionati.")
 
-        # Verifica se l'ordine degli anni di scoperta è crescente
         ordered_correctly = ordered_records['Anno di Scoperta'].is_monotonic_increasing
         if ordered_correctly and len(ordered_records) == len(sorted_items[0]['items']):
-            # Imposta lo stile per il messaggio di successo
-            st.markdown("<style>.green-background { background-color: lightgreen; padding: 10px; border-radius: 5px; }</style>", unsafe_allow_html=True)
-            # Mostra il messaggio di successo e i dettagli in verde
+            st.markdown("<div style='background-color:green;color:white;padding:10px;'>"
+                        "Hai indovinato l'ordine corretto!</div>", unsafe_allow_html=True)
             for _, row in ordered_records.iterrows():
-                st.markdown(f"<div class='green-background'>"
-                            f"{row['Descrizione Breve']} - {row['Anno di Scoperta']} - "
-                            f"{row['Descrizione Lunga']} - {row['Nome dell'Inventore']} - "
-                            f"{row['Paese di Origine']}</div>", unsafe_allow_html=True)
-            st.success("Hai indovinato l'ordine corretto!")
+                st.markdown(f"<div style='background-color:green;color:white;padding:10px;'>"
+                            f"{row['Descrizione Breve']} - {row['Anno di Scoperta']}</div>",
+                            unsafe_allow_html=True)
         else:
             st.error("Ordine non corretto. Riprova.")
