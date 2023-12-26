624172be901b8bddc861e938fdc0859cc64aba29
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-26 16:44:34 +0100

    Update test_csv.py

--- test_csv.py
+++ test_csv.py
@@ -3,13 +3,9 @@ import pandas as pd
 import streamlit as st
 
 
+
 df = pd.read_csv("https://raw.githubusercontent.com/frpeddis/TestApp1/main/Invenzioni.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
 # df = pd.read_excel(...)  # will work for Excel files
 
 st.title("Hello world!")  # add a title
 st.write(df)  # visualize my dataframe in the Streamlit app
-
-
-
-
-
