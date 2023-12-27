6222790354cc3b115083ce974b863e95b5e34e67
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-27 11:52:38 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -52,7 +52,7 @@ if not data.empty and len(data) >= 5:
         st.session_state['hint_indices'] = list(range(5))
 
     # Mostra le invenzioni casuali
-    items = [{'header': *'In alto i più antichi!'*, 'items': list(st.session_state['selected_records']['Descrizione Breve'])}]
+    items = [{'header': 'In alto i più antichi!', 'items': list(st.session_state['selected_records']['Descrizione Breve'])}]
     
     st.markdown("<div style='background-color: blue; color: white; padding: 14px; border: 6px solid white;'>"
                         f"🔄 Metti in ordine questi eventi!", unsafe_allow_html=True)
