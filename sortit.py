55d646586ffa4214bc01122afedbd99539c47d68
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-28 16:39:05 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -10,7 +10,7 @@ import time
 st.title('Riordina gli eventi! 😎')
 
 #  URL del file CSV su GitHub
-csv_url = 'https://raw.githubusercontent.com/frpeddis/TestApp1/main/events38.csv'
+csv_url = 'https://raw.githubusercontent.com/frpeddis/TestApp1/main/events37.csv'
 
 # Carica il file CSV da GitHub
 @st.cache
