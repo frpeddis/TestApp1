4b4e623b877fd7ca8170df5c92b9967e956da1f5
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-29 21:07:48 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -43,7 +43,7 @@ def reset_game():
     st.session_state['hint_indices'] = list(range(5))
 
 # URL del file CSV su GitHub
-csv_url = 'https://raw.githubusercontent.com/frpeddis/TestApp1/main/events36.csv'
+csv_url = 'https://raw.githubusercontent.com/frpeddis/TestApp1/main/events363.csv'
 data = load_data(csv_url)
 
 # Crea un container per il contenuto dell'app
