c34b85238178aabba40f06b5bb456eecf735c605
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-29 22:26:48 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -69,7 +69,7 @@ with st.container():
             st.session_state['hint_indices'] = list(range(5))
 
         # Mostra le invenzioni casuali
-        st.markdown("<div class='custom-box'>👆 Trascina in alto i segnalibri più antichi, 👇 in basso i più recenti!</div>", unsafe_allow_html=True)
+        st.markdown("<div class='custom-box'> <P>Riordina le pagine del tuo libro si Storia ! </P>👆 Trascina in alto i segnalibri più antichi, 👇 in basso i più recenti!</div>", unsafe_allow_html=True)
         
         items = [{'header': ' ', 'items': list(st.session_state['selected_records']['Descrizione Breve'])}]
         
