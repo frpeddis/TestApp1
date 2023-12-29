fea7adc623fedbbd12276e1a44a061da473b15d1
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-29 19:21:54 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -14,7 +14,7 @@ st.markdown(f"""
     <style>
     .stApp {{
         background-image: url('https://raw.githubusercontent.com/frpeddis/TestApp1/main/img41.jpeg');
-        background-size: 640px 640px;
+        background-size: 512px 256px;
     }}
     </style>
     """, unsafe_allow_html=True)
