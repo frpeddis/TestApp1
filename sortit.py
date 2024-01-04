8faf1674e3b9c82821fe0969d7f4b0201e7a7bf9
frpeddis <97230694+frpeddis@users.noreply.github.com>
2024-01-04 19:04:40 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -78,7 +78,7 @@ st.markdown(f"""
     .stButton > button:hover {{
         background-color: darkblue;  /* Darker green on hover */
         color: white;
-        border: 2px solid darkblue;
+        border: 2px blue;
     }}
     </style>
     """, unsafe_allow_html=True)
