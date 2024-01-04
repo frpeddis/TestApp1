c476c7bdf30b2337607d406131d8c37322ebcddd
frpeddis <97230694+frpeddis@users.noreply.github.com>
2024-01-04 18:27:44 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -68,7 +68,7 @@ st.markdown(f"""
         transform: translateY(-5px);  /* Lift effect on hover */
     }}
     .stButton > button {{
-        background-color: #03a1fc;  
+        background-color: lightgreen;  
         color: white;
         border-radius: 5px;
         padding: 10px 20px;
