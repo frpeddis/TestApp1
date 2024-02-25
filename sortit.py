a5f44afb4f457c92d616ccd4a217fbe2eb0b7aae
frpeddis <97230694+frpeddis@users.noreply.github.com>
2024-02-25 20:46:26 +0100

    Update sortit.py

--- sortit.py
+++ sortit.py
@@ -42,7 +42,7 @@ def load_data(url):
         return pd.DataFrame()
 
 # URL of the CSV file
-csv_url = 'https://raw.githubusercontent.com/frpeddis/TestApp1/main/events363.csv'
+csv_url = 'https://raw.githubusercontent.com/frpeddis/TestApp1/main/events3a.csv'
 
 # Load data
 data = load_data(csv_url)
