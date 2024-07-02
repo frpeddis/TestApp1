995d20862b89fd5374b3b56d4b8e927d74333b27
frpeddis <97230694+frpeddis@users.noreply.github.com>
2024-07-02 18:42:38 +0200

    Update troppoforte2.py

--- troppoforte2.py
+++ troppoforte2.py
@@ -1,53 +1,20 @@
 import random
+import matplotlib.pyplot as plt
 import calendar
 import tempfile
 import streamlit as st
 from datetime import datetime, timedelta
-import matplotlib.pyplot as plt
 from gtts import gTTS
 from num2words import num2words
 from io import BytesIO
+import plotly.graph_objects as go
 
-# Function to convert text to speech
-def text_to_speech(text, random_date):
-    today = datetime.now()
-    if random_date < today - timedelta(days=1):
-        prefix = "Che giorno era il "
-    else:
-        prefix = "Che giorno sarà il "
-    
-    tts = gTTS(text=f"{prefix} {text}", lang='it')
-    with tempfile.NamedTemporaryFile(suffix=".mp3", delete=True) as temp:
-        tts.save(temp.name)
-        temp.seek(0)
-        audio_data = temp.read()
-    audio_io = BytesIO(audio_data)
-    audio_io.seek(0)
-    return audio_io
-
-# Function to convert date to Italian words
+# Function to convert the date to Italian words
 def date_to_italian_words(date):
-    day = int(date.strftime("%d"))
-    month = date.strftime("%B")
-    year = int(date.strftime("%Y"))
-    day_words = num2words(day, lang='it')
-    year_words = num2words(year, lang='it')
-    month_map = {
-        'January': 'gennaio',
-        'February': 'febbraio',
-        'March': 'marzo',
-        'April': 'aprile',
-        'May': 'maggio',
-        'June': 'giugno',
-        'July': 'luglio',
-        'August': 'agosto',
-        'September': 'settembre',
-        'October': 'ottobre',
-        'November': 'novembre',
-        'December': 'dicembre'
-    }
-    month_words = month_map.get(month, '')
-    return f"{day_words} {month_words} {year_words}"
+    day = date.strftime("%d")
+    month = date.strftime("%m")
+    year = date.strftime("%Y")
+    return f"{day}/{month}/{year}"
 
 # Function to calculate a random date
 def calculate_random_date():
@@ -68,61 +35,131 @@ if 'question_start_time' not in st.session_state:
     st.session_state.question_start_time = datetime.now()
 if 'random_date' not in st.session_state:
     st.session_state.random_date = calculate_random_date()
+if 'selected_day_of_week' not in st.session_state:
+    st.session_state.selected_day_of_week = None
 if 'button_label' not in st.session_state:
-    st.session_state.button_label = "Check Question 1"
+    st.session_state.button_label = "Check Question 1/NEXT"
 if 'time_list' not in st.session_state:
     st.session_state.time_list = []
 if 'show_summary' not in st.session_state:
     st.session_state.show_summary = False
 
 # Streamlit app title
-st.title(":sunglasses: What day is it? Random date 🎲")
+st.title(":sunglasses: What's the day? 🎲")
 
-# Convert the date to Italian words
-date_words = date_to_italian_words(st.session_state.random_date)
+# Adding an option for "Silent mode"
+silent_mode = st.checkbox("Silent mode")
+if not silent_mode:
+    # Function to convert text to speech using gTTS
+    def text_to_speech(text, random_date):
+        today = datetime.now()
+        prefix = "Che giorno era il " if random_date < today - timedelta(days=1) else "Che giorno sarà il "
+        
+        # Always select Italian as the language
+        selected_lang = 'it'
+        
+        tts = gTTS(text=f"{prefix} {text}", lang=selected_lang)
+        
+        with tempfile.NamedTemporaryFile(suffix=".mp3", delete=True) as temp:
+            tts.save(temp.name)
+            temp.seek(0)
+            audio_data = temp.read()
+        
+        audio_io = BytesIO(audio_data)
+        audio_io.seek(0)
+        return audio_io
 
-# Text to speech with the modified function
-audio_io = text_to_speech(date_words, st.session_state.random_date)
+    # Convert the date to Italian words
+    date_words = date_to_italian_words(st.session_state.random_date)
 
-# Streamlit audio player
-audio_io.seek(0)
-audio_bytes = audio_io.read()
-st.audio(audio_bytes, format='audio/wav')
+    # Text to speech with the modified function
+    audio_io = text_to_speech(date_words, st.session_state.random_date)
 
-# User selection for day of the week
-selected_day_of_week = st.selectbox(
-    f"Select the day of the week for question {st.session_state.question_count + 1}:",
-    list(calendar.day_name),
-)
+    # Streamlit audio player
+    audio_io.seek(0)
+    audio_bytes = audio_io.read()
+    st.audio(audio_bytes, format='audio/mp3')
 
-# Button to confirm the selection
-check_button = st.button(st.session_state.button_label)
+# Display the random date only in "Silent mode" and in the format "dd/mm/yyyy"
+if silent_mode:
+    date_displayed = date_to_italian_words(st.session_state.random_date)
+    st.write(f"Random date: {date_displayed}")
 
-# Logic for checking the answer
-if check_button:
-    day_of_week = calendar.day_name[st.session_state.random_date.weekday()]
-    if selected_day_of_week == day_of_week:
-        st.balloons()
-        st.success(f"{day_of_week} is OK! :thumbsup:")
+# Creating two columns for the layout
+left_column, right_column = st.columns(2)
+
+# In the left column, place the day selection radio buttons
+with left_column:
+    day_options = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    st.session_state.selected_day_of_week = st.radio("Seleziona:", day_options, key="day_radio")
+
+    # Button to confirm the selection and check the answer
+    # Display the button only if question_count is less than 5
+    if st.session_state.question_count < 5:
+        check_button = st.button(st.session_state.button_label)
     else:
-        st.session_state.error_count_list[st.session_state.question_count] += 1
-        st.error(f"{day_of_week} is the right day! :coffee:")
+        check_button = False
 
-    question_time_taken = (
-        datetime.now() - st.session_state.question_start_time
-    ).total_seconds()
-    st.session_state.total_time += question_time_taken
-    st.session_state.time_list.append(question_time_taken)
-    st.session_state.question_count += 1
-    st.session_state.question_start_time = datetime.now()
-    st.session_state.random_date = calculate_random_date()
-    st.session_state.button_label = f"Check Question {st.session_state.question_count + 1} / NEXT"
+# Function to create the pie chart
+def create_pie_chart(selected_day, correct_day=None, is_checked=False):
+    days_short = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    full_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    colors = ['lightgray'] * 7
+
+    if selected_day:
+        selected_day_index = full_days.index(selected_day)
+        colors[selected_day_index] = 'violet'
+
+        if is_checked:
+            if correct_day == selected_day:
+                colors[selected_day_index] = 'lightgreen'
+            else:
+                colors[selected_day_index] = 'red'
+                correct_day_index = full_days.index(correct_day)
+                colors[correct_day_index] = 'lightgreen'
+
+    fig = go.Figure(data=[go.Pie(labels=days_short, values=[1]*7, marker=dict(colors=colors), hole=.4, direction='clockwise')])
+    fig.update_traces(textinfo='label', textfont_size=15, hoverinfo='none')
+    fig.update_layout(
+        showlegend=False,
+        height=160,
+        width=160,
+        margin=dict(l=8, r=8, t=8, b=8)
+    )
+
+    return fig
+
+# In the right column, display the pie chart
+with right_column:
+    day_of_week = calendar.day_name[st.session_state.random_date.weekday()]
+    fig = create_pie_chart(st.session_state.selected_day_of_week, day_of_week if check_button else None, check_button)
+    st.plotly_chart(fig, use_container_width=True)
+
+    if check_button:
+        if st.session_state.selected_day_of_week == day_of_week:
+            st.balloons()
+            st.success(f"{day_of_week} is right! :thumbsup:")
+        else:
+            st.session_state.error_count_list[st.session_state.question_count] += 1
+            st.snow()
+            st.error(f"{day_of_week} was right! :coffee:")
+
+        question_time_taken = (
+            datetime.now() - st.session_state.question_start_time
+        ).total_seconds()
+        st.session_state.total_time += question_time_taken
+        st.session_state.time_list.append(question_time_taken)
+        st.session_state.question_count += 1
+        st.session_state.question_start_time = datetime.now()
+        st.session_state.random_date = calculate_random_date()
+        st.session_state.button_label = f"Check Question {st.session_state.question_count + 1} / NEXT"
 
 # Show summary after 5 questions
 if st.session_state.question_count >= 5:
     st.session_state.show_summary = True
 
 if st.session_state.show_summary:
+    
     average_time = st.session_state.total_time / 5
     st.write(f"Total time taken for all 5 questions: {round(st.session_state.total_time, 2)} seconds")
     st.write(f"Shortest time taken: {round(min(st.session_state.time_list), 2)} seconds")
@@ -145,11 +182,10 @@ if st.session_state.show_summary:
     plt.legend()
     st.pyplot(plt)
 
-    
-    if st.button("Restart"):  # New button
+    if st.button("Restart"):
         st.session_state.question_count = 0
         st.session_state.total_time = 0.0
         st.session_state.time_list = []
-        st.session_state.button_label = "Check Question 1"
-        st.session_state.show_summary = False  # Reset the summary display
-        st.experimental_rerun()  # Rerun the app to reset the display
+        st.session_state.button_label = "Check Question 1 / NEXT"
+        st.session_state.show_summary = False
+        st.experimental_rerun()
